c09f5f0a71b2dd88abf228cbaf527739d0eab84c
joannembl <jmbliberato@gmail.com>
2022-10-07 12:24:38 -0700

    feat(tags): added total records + display entries

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import { FormControl, MenuItem, Select } from '@mui/material';
+import React, { useState } from 'react'
 import NavBar from '../components/NavBar';
 import SearchBar from '../components/SearchBar';
 
 function Home() {
+  const [entries, setEntries] = useState(20);
+
+  const handleEntriesChange = (e) => {
+    setEntries(e.target.value);
+  }
+
   return (
     <div>
         <div className='navigation'>
             <NavBar />
             <SearchBar />
+        </div>
+        <div className='tags'>
+            <span className='total-records'>Total Records Found: 0</span>
+            <div className='display-entries'>
+                <span>Display: </span>
+                <FormControl sx={{ m: 1, minWidth: 150 }}>
+                    <Select
+                        value={entries}
+                        onChange={handleEntriesChange}
+                    >
+                        <MenuItem value={5}>5 entries</MenuItem>
+                        <MenuItem value={10}>10 entries</MenuItem>
+                        <MenuItem value={20}>20 entries</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
         </div>
     </div>
   )
